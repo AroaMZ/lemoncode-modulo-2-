849e577f30fa4d0cfaa2a2e19d85a01a96273318
Aroa Mendez <aroazmendez@gmail.com>
2023-01-22 19:54:51 +0100

    Moludo 2

---
+++ js/demo.js
@@ -0,0 +1,36 @@
+var hoteles = {
+    malkorra :  {
+        nombre: "Malkorra",
+        ubicacion: "San Sebastian",
+        img: "https://turismo.euskadi.eus/contenidos/a_alojamiento/0000095481_a1_rec_turismo/es_95481/images/GL_malkorrahotelservicios11.jpg",
+        
+    },
+
+    londres : {
+        nombre: "Londres",
+        ubicacion: "San Sebastian",
+        img: "https://groupcorner.com/uploads/HOTELS/hotel_10291/diaporama/_-image_3_1120/1000x548_C_image_3_1120.jpg",
+       
+    },
+};
+
+var estrellas = {
+    1: "&#9733",
+    2: "&#9733 &#9733",
+    3: "&#9733 &#9733 &#9733",
+    4: "&#9733 &#9733 &#9733 &#9733",
+    5: "&#9733 &#9733 &#9733 &#9733 &#9733",
+};
+ 
+
+var nombreHotel = prompt("Introduce el nombre del hotel: Malkorra o Londres");
+document.getElementById("nombre").innerHTML = hoteles[nombreHotel].nombre;
+document.getElementById("ubicacion").innerHTML = hoteles[nombreHotel].ubicacion;
+document.getElementById("hotel-img").src = hoteles[nombreHotel].img;
+
+var puntuacion = prompt("Que puntuacion le das al hotel? del 1 al 5");
+document.getElementById("estrellas").innerHTML = estrellas[puntuacion];
+document.getElementById("estrellas").style.color =  "#FFFF00";
+
+var resena = confirm("Quieres que la reseña sea anonima?");
+document.getElementById("resena").checked = resena;
