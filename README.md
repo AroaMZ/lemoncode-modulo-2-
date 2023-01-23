66f110f0814f5e7b57e32b742afab36ce1cdcc8a
Aroa Mendez <aroazmendez@gmail.com>
2023-01-23 10:16:43 +0100

    Modificación de las mayusculas de los objetos.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # lemoncode-modulo-2-
+Ejercicio correspondiente al bootcamp lemon code modulo 2.
+El objetivo es:
+1. Construir la estructura HTML
+2. Crear código JavaScript
+3. Mostrar puntuación del usuario.
+4. Preguntar al usuario si quiere enviar la reseña de forma anónima
+5. Que la puntuación dada por el usuario sea mostrando el típico rating de estrellas.
--- js/demo.js
+++ js/demo.js
@@ -1,12 +1,12 @@
 var hoteles = {
-    malkorra :  {
+    Malkorra :  {
         nombre: "Malkorra",
         ubicacion: "San Sebastian",
         img: "https://turismo.euskadi.eus/contenidos/a_alojamiento/0000095481_a1_rec_turismo/es_95481/images/GL_malkorrahotelservicios11.jpg",
         
     },
 
-    londres : {
+    Londres : {
         nombre: "Londres",
         ubicacion: "San Sebastian",
         img: "https://groupcorner.com/uploads/HOTELS/hotel_10291/diaporama/_-image_3_1120/1000x548_C_image_3_1120.jpg",
